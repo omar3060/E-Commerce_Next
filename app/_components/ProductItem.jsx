e4dfb84c06ea97808af72f00b9bdbd14ebabb0ce
omar mahmoud <omarmahmoudcs@gmail.com>
2025-02-23 17:05:37 +0200

    Add icon

--- app/_components/ProductItem.jsx
+++ app/_components/ProductItem.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import Image from 'next/image'
+import {List} from 'lucide-react'
 const ProductItem = ({product}) => {
   return (
     <div>
       <Image src={product?.banner?.url} alt={`${product.title} banner`} width={400} height={350} className="rounded-t-lg h-[170px] object-cover"/>
       <div className="p-3">
         <h2 className="text-[16px] md:text-[20px] font-medium">{product?.title}</h2>
-        <h2 className="text-[14px] text-gray-400">{product?.category}</h2>
+        <h2 className="text-[14px] text-gray-400"><List /> {product?.category}</h2>
         
         
         
