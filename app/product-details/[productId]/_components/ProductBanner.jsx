7ee1a198d351c01b67fed84ba5aa38642764ab8f
omar mahmoud <omarmahmoudcs@gmail.com>
2025-03-16 21:33:29 +0200

    Adding Skeleton effect for product details page

--- app/product-details/[productId]/_components/ProductBanner.jsx
+++ app/product-details/[productId]/_components/ProductBanner.jsx
@@ -1,17 +1,21 @@
-import React from 'react'
-import Image from 'next/image'
+import React from "react";
+import Image from "next/image";
 function ProductBanner({ product }) {
-	return (
-		<div>
-				<Image
-					src={product?.banner?.url}
-					alt='product-details-banner'
-					width={400}
-					height={400}
-					className='rounded-lg'
-				/>
-		</div>
-	)
+  return (
+    <div>
+      {product ? (
+        <Image
+          src={product?.banner?.url}
+          alt="product-details-banner"
+          width={400}
+          height={400}
+          className="rounded-lg"
+        />
+      ) : (
+        <div className="w-[336px] h-[189px] bg-slate-200 animate-pulse"></div>
+      )}
+    </div>
+  );
 }
 
-export default ProductBanner
+export default ProductBanner;
--- app/product-details/[productId]/_components/ProductInfo.jsx
+++ app/product-details/[productId]/_components/ProductInfo.jsx
@@ -1,18 +1,37 @@
-import { AlertOctagon, BadgeCheck, ShoppingCart } from 'lucide-react'
-import React from 'react'
+import { AlertOctagon, BadgeCheck, ShoppingCart } from "lucide-react";
+import React from "react";
+import SkeletonProductInfo from "./SkeletonProductInfo";
 
-const ProductInfo = ({product}) => {
+const ProductInfo = ({ product }) => {
   // console.log(product.title)
   return (
     <div>
-      <h2 className='text-[20px]'>{product?.title}</h2>
-      <h2 className='text-[15px] text-gray-400'>{product?.category}</h2>
-      <h2 className='text-[11px] mt-5'>{product?.description[0]?.children[0].text}</h2>
-      <h2 className='text-[11px] text-gray-500 flex gap-2 items-center mt-2'>{product?.instantDelivery ? <BadgeCheck className="text-green-500 h-5 w-5"/>: <AlertOctagon className=''/>}Eligible For Instant Delivery</h2>
-    <h2 className='text-[32px] text-primary mt-3'>$ {product?.price}</h2>
-    <button className='flex gap-2 rounded-lg bg-primary hover:bg-teal-500 text-white p-3'><ShoppingCart/>Add To Cart</button>
+      {product ? (
+        <div>
+          <h2 className="text-[20px]">{product?.title}</h2>
+          <h2 className="text-[15px] text-gray-400">{product?.category}</h2>
+          <h2 className="text-[11px] mt-5">
+            {product?.description[0]?.children[0].text}
+          </h2>
+          <h2 className="text-[11px] text-gray-500 flex gap-2 items-center mt-2">
+            {product?.instantDelivery ? (
+              <BadgeCheck className="text-green-500 h-5 w-5" />
+            ) : (
+              <AlertOctagon className="" />
+            )}
+            Eligible For Instant Delivery
+          </h2>
+          <h2 className="text-[32px] text-primary mt-3">$ {product?.price}</h2>
+          <button className="flex gap-2 rounded-lg bg-primary hover:bg-teal-500 text-white p-3">
+            <ShoppingCart />
+            Add To Cart
+          </button>
+        </div>
+      ) : (
+        <SkeletonProductInfo />
+      )}
     </div>
-  )
-}
+  );
+};
 
-export default ProductInfo
+export default ProductInfo;
